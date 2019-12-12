948511b545ec61782d19763b90f021758e587e3b
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-13 02:32:24 +0530

    Update Operator.js

--- Operators.js
+++ Operators.js
@@ -6,4 +6,7 @@ number1 += number2;
 number1++;
 //number1--;
 
+//var number1 = 4+3*5;  //The multiplication and division operators take precedence over addition and subtraction operators
+
+
 document.write (number1);
