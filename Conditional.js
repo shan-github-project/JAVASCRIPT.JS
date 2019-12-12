5e4d4981af6513713e5e11d91fdcd162ada65205
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-13 03:20:12 +0530

    Create Conditional.js
    Use same HTML file

---
+++ Conditional.js
@@ -0,0 +1,24 @@
+var food = "Fast Food";  //change this to see effect of if and else
+
+if (food == "Apple") {
+
+alert ("we ate some fruit and specifically 
+
+an Apple");
+}
+
+else if (food == "Meat") {
+
+alert ("we ate some meat");
+}
+
+else if (food == "Fast Food") {
+
+alert ("we ate some Fast Food");
+}
+
+else{
+
+alert ("we ate something else");
+
+}
