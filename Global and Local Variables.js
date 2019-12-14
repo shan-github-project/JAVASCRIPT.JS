329d98d11ac7af5e103a67057b182a48e2ac89fc
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-14 22:28:01 +0530

    Create Global and Local Variables.js
    Local Variable became global variable when we remove VAR  as javascript consider local value  globally  then local value can call outside function
    
    Global variable always there untill page is in function or reloaded

---
+++ Global and Local Variables.js
@@ -0,0 +1,14 @@
+var name="Henry"; //global variable
+
+function printName() {
+
+ var name2="sarah";    //local variable
+//document.write(name2); // To call local variable
+
+}
+
+printName();  //call function
+
+//document.write(name);  // To call global varaible
+  
+// document.write(name2); // when local variable become global variable
