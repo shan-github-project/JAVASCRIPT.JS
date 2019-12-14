60d6950599b116340a6de082811850ac255117f7
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-14 22:51:55 +0530

    Create pass by value.js

---
+++ Pass by Value.js
@@ -0,0 +1,10 @@
+function batting(player, distance) {
+
+var more= player + " hit the ball " + distance + " feet ";
+document.write(more);
+
+}
+
+batting( "steve", 351);  // we pass in 
+
+arguments to parameters
