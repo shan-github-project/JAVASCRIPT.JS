a1101bdfb5da4809eddf3d4fec0943ec257c372e
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-15 00:21:29 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,17 @@
+var roads = ["stone", "dirt", "cement", "tar",];
+
+roads[1] = "cobblestone";
+
+var roadTravelled = roads[1];
+
+document.write(roadTravelled); 
+
+
+/* Long version
+   var roads = [];
+   roads[0] = "stone";
+   roads[1] = "dirt";
+   roads[2] = "cement";
+   roads[3] = "tar";
+
+*/
