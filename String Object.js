bc05f1df74255187e546d3a8266f60dc0f5e93d1
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-15 20:44:22 +0530

    Create String Object.js
    length property- count string from 0 to last word+1, here last word+1 throws extra unit of character 
                                 count empty space.

---
+++ String Object.js
@@ -0,0 +1,11 @@
+var hello = "Hello how are you doing";
+
+//hello = hello.length;
+//hello = hello.toUpperCase();
+//hello = hello.charAt(4);
+//hello = hello.replace('doing','today');
+//hello = hello.bold();
+
+hello = hello.italics();
+
+document.write(hello);
