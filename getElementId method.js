4fc9495f0a45c5051c4334ab43aa9426cc6b27b7
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-18 07:11:28 +0530

    Create getElementId method.js
    1.Need to grab the elementId we can do that with get element by Id method
    2.Once we have the Id we are good to go and we can start to style the element with the style object which is another object in the DOM
    
    CSS                                                   JS
    
    color                                                color
    border                                             border
    margin                                           margin
    
    background-color                        backgroundColor
    font-style                                      fontStyle
    text-decoration                            textdecoration
    
    padding-left                                 padding-left
    border-left                                    border-left

---
+++ getElementId method.js
@@ -0,0 +1,20 @@
+function changeStyle() {
+
+
+//var text = document.getElementById 
+
+("para1").style.color = "blue";
+
+//var text = document.getElementById 
+
+("para1").style.backgroundColor = "red";
+
+
+var text
+ = document.getElementById 
+
+("para1").style.fontStyle = "italic";
+
+
+
+}
