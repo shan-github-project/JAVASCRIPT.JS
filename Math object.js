42ac987d151d3b4678bad407f893dc35a2e00325
shan-github-project <57130457+shan-github-project@users.noreply.github.com>
2019-12-18 05:56:50 +0530

    Create Math object.js

---
+++ Math object.js
@@ -0,0 +1,18 @@
+//var number = 4.7;
+
+
+
+//var newNumber = 26 +  Math.round(number);
+
+
+
+//var newNumber = 26 + Math.ceil(number);
+
+//var newNumber = 26 + Math.floor(number);
+
+//document.write(newNumber);
+
+
+var number = Math.sqrt(16);
+
+document.write(number);
